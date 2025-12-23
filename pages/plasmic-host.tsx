0e6609ae044207031c3db3622d5fb041f00e3cd3
Disuko <disukomusic@gmail.com>
2025-12-22 20:01:51 -0500

    hmmmm

--- pages/plasmic-host.tsx
+++ pages/plasmic-host.tsx
@@ -1,13 +1,7 @@
 import * as React from 'react';
 import { PlasmicCanvasHost } from '@plasmicapp/loader-nextjs';
-import { PLASMIC } from '@/plasmic-init';
+import { PLASMIC } from '../plasmic-init';
 
 export default function PlasmicHost() {
-  // Use CanvasHost only for dev preview inside Plasmic editor
-  if (process.env.NODE_ENV === "development") {
-    return <PlasmicCanvasHost />;
-  }
-
-  // Production / Vercel: serve actual registered components
-  return <PlasmicHost />;
+  return PLASMIC && <PlasmicCanvasHost />;
 }
