07e508472137303b6632a6030d491934692cfd38
Disuko <disukomusic@gmail.com>
2025-12-22 19:54:25 -0500

    [Fix] code components via vercel deployment(?)

--- pages/plasmic-host.tsx
+++ pages/plasmic-host.tsx
@@ -3,5 +3,11 @@ import { PlasmicCanvasHost } from '@plasmicapp/loader-nextjs';
 import { PLASMIC } from '@/plasmic-init';
 
 export default function PlasmicHost() {
-  return PLASMIC && <PlasmicCanvasHost />;
+  // Use CanvasHost only for dev preview inside Plasmic editor
+  if (process.env.NODE_ENV === "development") {
+    return <PlasmicCanvasHost />;
+  }
+
+  // Production / Vercel: serve actual registered components
+  return <PlasmicHost />;
 }
