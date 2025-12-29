43d1ff70a5c0394a26b7083b38529c0568f33a12
Disuko <disukomusic@gmail.com>
2025-12-28 21:06:34 -0500

    callback home page link qol

--- pages/oauth/callback.tsx
+++ pages/oauth/callback.tsx
@@ -3,7 +3,7 @@ export default function OAuthCallback() {
     return (
         <div style={{ padding: 24 }}>
             <h1>Signing you in…</h1>
-            <p>If this doesn’t redirect automatically, go back to the app tab.</p>
+            <a href="/miisky">If this doesn’t redirect automatically, click here.</a>
         </div>
     );
 }
