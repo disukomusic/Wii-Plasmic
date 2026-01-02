663a987faf4bc1aeb6bd5876f514dbac505df62e
Disuko <disukomusic@gmail.com>
2026-01-02 15:01:53 -0500

    [Fix] fix fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 ﻿
 import type {AppProps} from 'next/app';
 import {BlueskyAuthProvider} from '@/lib/BlueskyAuthProvider';
+import '../styles/globals.css'
 
 export default function MyApp({Component, pageProps}: AppProps) {
     return (
